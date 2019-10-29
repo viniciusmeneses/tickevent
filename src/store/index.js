6f0e03094ffd85bc732a5e95cd6fa6925eeabf01
Vinicius Meneses <vinicius.meneses@tagview.com.br>
2019-10-29 14:03:12 -0300

    Configura redux-logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import logger from 'redux-logger';
 
 import reducers from './ducks';
 import sagas from './sagas';
 
 const middlewares = [];
+
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
+
 const sagaMiddleware = createSagaMiddleware();
 middlewares.push(sagaMiddleware);
 

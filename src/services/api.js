4b3d6167b4f54cbb764f764974a023ba35fcf82f
viniciusmeneses <vinicius.meneses04@gmail.com>
2019-10-30 23:57:44 -0300

    Cria helper para autentiacao

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,16 @@ import Config from 'react-native-config';
 
 const api = axios.create({
   baseURL: Config.API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export const configWithAuth = (token, config = {}) => ({
+  ...config,
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
 });
 
 export default api;

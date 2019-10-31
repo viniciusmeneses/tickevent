c645a681b41f877a53ce6613ab4b0c088e6fb3c6
Vinicius Meneses <vinicius.meneses@tagview.com.br>
2019-10-31 15:44:49 -0300

    Implementa saga de login e de obter dados do usuário

--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -1,7 +1,35 @@
-import { all, takeLatest } from 'redux-saga/effects';
+import { all, takeLatest, call, put } from 'redux-saga/effects';
+import jwtDecode from 'jwt-decode';
 
-function* login() {}
+import {
+  Types,
+  loginSuccess,
+  loadUserData,
+  loadUserDataSuccess,
+} from '../../store/ducks/auth';
+import apiService, { configWithAuth } from '../../services/api';
+
+function* loginSaga({ payload }) {
+  try {
+    const { data } = yield call(apiService.post, '/login', { payload });
+    const token = data.accessToken;
+    yield all([put(loginSuccess(token)), put(loadUserData(token))]);
+  } catch (e) {}
+}
+
+function* loadUserDataSaga({ payload }) {
+  try {
+    const { sub: userId } = jwtDecode(payload.token);
+    const { data } = yield call(
+      apiService.get,
+      `/users/${userId}`,
+      configWithAuth(payload.token)
+    );
+    yield put(loadUserData(loadUserDataSuccess(data.user)));
+  } catch (e) {}
+}
 
 export default all([
-  takeLatest(login),
+  takeLatest(Types.LOGIN, loginSaga),
+  takeLatest(Types.LOAD_USER_DATA, loadUserDataSaga),
 ]);

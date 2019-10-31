3fcb8a3c2d27d801a0b2ed8f007d14d182276c86
viniciusmeneses <vinicius.meneses04@gmail.com>
2019-10-30 23:05:24 -0300

    Implementa layout da tela de login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,10 +1,69 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react';
 
-// import { Container } from './styles';
+import Input from '../../components/Input';
+import SubmitButton from '../../components/SubmitButton';
+import Logo from '../../components/Logo';
+import ScrollContainer from '../../components/ScrollContainer';
 
-export default function Login() {
-  return (
-    <View />
-  );
+import {
+  Container,
+  FormContainer,
+  RegisterPageButton,
+  RegisterPageButtonText,
+  LogoContainer,
+} from './styles';
+
+export default class Login extends Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
+  navigateToRegister = () => {
+    const { navigation } = this.props;
+    navigation.navigate('Register');
+  };
+
+  render() {
+    const { email, password } = this.state;
+
+    return (
+      <ScrollContainer>
+        <Container>
+          <LogoContainer>
+            <Logo />
+          </LogoContainer>
+
+          <FormContainer>
+            <Input
+              placeholder="E-mail"
+              onChangeText={text => this.setState({ email: text })}
+              value={email}
+              autoCapitalize="none"
+              autoCorrect={false}
+              keyboardType="email-address"
+              autoCompleteType="email"
+            />
+            <Input
+              placeholder="Senha"
+              onChangeText={text => this.setState({ password: text })}
+              value={password}
+              secureTextEntry
+              autoCapitalize="none"
+              autoCorrect={false}
+              autoCompleteType="password"
+            />
+
+            <SubmitButton text="Acessar" />
+          </FormContainer>
+
+          <RegisterPageButton onPress={this.navigateToRegister}>
+            <RegisterPageButtonText>
+              Criar uma nova conta
+            </RegisterPageButtonText>
+          </RegisterPageButton>
+        </Container>
+      </ScrollContainer>
+    );
+  }
 }
---
+++ src/pages/Login/styles.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components/native';
+
+export const LogoContainer = styled.View`
+  margin-top: auto;
+`;
+
+export const Container = styled.KeyboardAvoidingView`
+  flex: 1;
+  background-color: #fbfbfb;
+  justify-content: center;
+  align-items: center;
+  padding: 15px 0;
+`;
+
+export const FormContainer = styled.View`
+  margin-top: 50px;
+  margin-bottom: 15px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const RegisterPageButton = styled.TouchableOpacity`
+  margin-top: auto;
+`;
+
+export const RegisterPageButtonText = styled.Text`
+  font-family: 'Roboto';
+  font-size: 16px;
+  color: #ff5757;
+  text-transform: uppercase;
+  font-weight: bold;
+`;

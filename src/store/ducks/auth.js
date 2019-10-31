19680d9e37c814050b82b7534ff3f30211ebeb30
viniciusmeneses <vinicius.meneses04@gmail.com>
2019-10-31 18:02:38 -0300

    Corrige actions creators e reduce de autenticacao

--- src/store/ducks/auth.js
+++ src/store/ducks/auth.js
@@ -1,29 +1,34 @@
 export const Types = {
   LOGIN: 'auth/LOGIN',
   LOGIN_SUCCESS: 'auth/LOGIN_SUCCESS',
+  LOGIN_ERROR: 'auth/LOGIN_ERROR',
   LOAD_USER_DATA: 'auth/LOAD_USER_DATA',
   LOAD_USER_DATA_SUCCESS: 'auth/LOAD_USER_DATA_SUCCESS',
 };
 
 export const login = (email, password) => ({
-  type: Types.LOGIN_REQUEST,
+  type: Types.LOGIN,
   payload: {
     email,
     password,
   },
 });
 
-export const loginSuccess = jwtToken => ({
+export const loginSuccess = token => ({
   type: Types.LOGIN_SUCCESS,
   payload: {
-    jwtToken,
+    token,
   },
 });
 
-export const loadUserData = jwtToken => ({
+export const loginError = () => ({
+  type: Types.LOGIN_ERROR,
+});
+
+export const loadUserData = token => ({
   type: Types.LOAD_USER_DATA,
   payload: {
-    jwtToken,
+    token,
   },
 });
 
@@ -37,14 +42,20 @@ export const loadUserDataSuccess = user => ({
 const initialState = {
   token: null,
   user: {},
+  isLogging: false,
+  isRegistering: false,
 };
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
+    case Types.LOGIN:
+      return { ...state, isLogging: true };
     case Types.LOGIN_SUCCESS:
-      return { ...state, token: action.payload.jwtToken };
+      return { ...state, token: action.payload.token };
+    case Types.LOGIN_ERROR:
+      return { ...state, isLogging: false };
     case Types.LOAD_USER_DATA_SUCCESS:
-      return { ...state, user: action.payload.user };
+      return { ...state, user: action.payload.user, isLogging: false };
     default:
       return state;
   }

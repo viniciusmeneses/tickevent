9f3abc5eaf205ef002f1a4da7b636bde6bfeb378
viniciusmeneses <vinicius.meneses04@gmail.com>
2019-10-31 23:59:09 -0300

    Troca SwitchNavigator por SwitchNavigator animado

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,20 +1,24 @@
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createAppContainer } from 'react-navigation';
+import createAnimatedSwitchNavigator from 'react-navigation-animated-switch';
 
 import AuthSplashPage from '../pages/AuthSplash';
 import AuthNavigator from './auth';
 import AppNavigator from './app';
 
-const RootNavigator = createSwitchNavigator({
-  Splash: {
-    screen: AuthSplashPage,
+const RootNavigator = createAnimatedSwitchNavigator(
+  {
+    Splash: {
+      screen: AuthSplashPage,
+    },
+    Auth: {
+      screen: AuthNavigator,
+    },
+    App: {
+      screen: AppNavigator,
+    },
   },
-  Auth: {
-    screen: AuthNavigator,
-  },
-  App: {
-    screen: AppNavigator,
-  },
-});
+  {}
+);
 
 const AppContainer = createAppContainer(RootNavigator);
 export default AppContainer;

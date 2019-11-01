a01c7465bc7c9dba470fe0ff7e669e9285ce6b8c
viniciusmeneses <vinicius.meneses04@gmail.com>
2019-11-01 00:01:04 -0300

    Configura statusbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import AppContainer from './navigation';
+import AuthSplash from './pages/AuthSplash';
+
 import navigationService from './services/navigation';
 import { store, persistor } from './store';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <PersistGate persistor={persistor}>
+      <PersistGate persistor={persistor} loading={<AuthSplash />}>
+        <StatusBar backgroundColor="#ff5757" />
         <AppContainer
           ref={navigator => navigationService.setNavigator(navigator)}
         />

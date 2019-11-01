7f22b5a385cf089b06a44a112e163ddd1d46da53
viniciusmeneses <vinicius.meneses04@gmail.com>
2019-10-31 23:59:52 -0300

    Alinha corretamente label das tabs

--- src/navigation/app.js
+++ src/navigation/app.js
@@ -2,10 +2,23 @@ import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 import HomePage from '../pages/Home';
 
-const AppNavigator = createBottomTabNavigator({
-  Home: {
-    screen: HomePage,
+const AppNavigator = createBottomTabNavigator(
+  {
+    Home: {
+      screen: HomePage,
+    },
   },
-});
+  {
+    tabBarOptions: {
+      tabStyle: {
+        justifyContent: 'center',
+      },
+      labelStyle: {
+        fontSize: 18,
+      },
+      showIcon: false,
+    },
+  }
+);
 
 export default AppNavigator;
